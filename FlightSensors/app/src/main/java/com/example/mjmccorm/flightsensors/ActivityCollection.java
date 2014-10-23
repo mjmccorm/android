9d51f8c8969eb6c71ec007ae21b656c2c0e09a97
Matt McCormick <mjmccorm@gmail.com>
2014-10-22 22:18:43 -0400

    Added new activity - pressure

--- FlightSensors/app/src/main/java/com/example/mjmccorm/flightsensors/ActivityCollection.java
+++ FlightSensors/app/src/main/java/com/example/mjmccorm/flightsensors/ActivityCollection.java
@@ -1,42 +1,56 @@
 package com.example.mjmccorm.flightsensors;
 
+import java.util.List;
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
-
-public class ActivityCollection extends Activity {
+public class ActivityCollection extends Activity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mPressure;
+    TextView myTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
-
-        int age = getIntent().getIntExtra("user-age", -1);
-
-        TextView myTextView = (TextView)findViewById(R.id.lblActCol);
-        myTextView.setText("" + age);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_collection, menu);
-        return true;
+        myTextView = (TextView)findViewById(R.id.lblActCol);
+        // Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
     }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        float millibars_of_pressure = event.values[0];
+        // Do something with this sensor data.
+
+        myTextView.setText("Pressure:" + millibars_of_pressure);
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
 }
